40ad91c1418767c24bee625249559531fa8eb1be
João Vicente <joao.vicente@campus.ul.pt>
2020-11-16 14:48:54 +0000

    Bugs fixed. Interface updated

---
+++ src/app/pipes/custom-date.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Pipe({
+  name: 'customDate'
+})
+export class CustomDatePipe implements PipeTransform {
+
+  constructor(private readonly translate: TranslateService) {}
+
+  transform(value: string): string {
+    const split = value.split('/');
+    split[1] = this.translate.instant('MONTHS.' + this.fixMonth(split[1]));
+    return split.join(' ');
+  }
+
+  private fixMonth(month: string): string {
+    if (month[0] === '0') {
+      month = month.substring(1, 2);
+    }
+
+    return month;
+  }
+}
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { GetConformPipe } from './get-conform.pipe';
 import { ToFixedPipe } from './to-fixed.pipe';
 import { HtmlPipe } from './html.pipe';
+import { CustomDatePipe } from './custom-date.pipe';
 
 @NgModule({
-  declarations: [GetConformPipe, ToFixedPipe, HtmlPipe],
+  declarations: [GetConformPipe, ToFixedPipe, HtmlPipe, CustomDatePipe],
   providers: [GetConformPipe, ToFixedPipe, HtmlPipe],
   imports: [CommonModule],
-  exports: [GetConformPipe, ToFixedPipe, HtmlPipe]
+  exports: [GetConformPipe, ToFixedPipe, HtmlPipe, CustomDatePipe]
 })
 export class PipesModule { }
