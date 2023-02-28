c7d377b96512ee5d2f70616563b1f2a4e5608432
Estriga97 <fc47839@alunos.fc.ul.pt>
2023-02-28 11:12:41 +0000

    bug fixes

--- src/app/observatory-numbers/observatory-numbers.component.ts
+++ src/app/observatory-numbers/observatory-numbers.component.ts
@@ -33,78 +33,108 @@ export class ObservatoryNumbersComponent implements OnInit {
       this.declarations.total.apps.partial +
       this.declarations.total.apps.not_conform;
 
+    const totalDeclarationsWeb =
+      this.declarations.total.websites.conform +
+      this.declarations.total.websites.partial +
+      this.declarations.total.websites.not_conform;
+
+    const totalDeclarationsApps =
+      this.declarations.total.apps.conform +
+      this.declarations.total.apps.partial +
+      this.declarations.total.apps.not_conform + 1;//FIXME
+
     const declarationBars = document.querySelectorAll(".declaration .bar");
 
-    this.fillBar(<HTMLElement>declarationBars[0], "#15ac51", 100);
+    this.fillBar(<HTMLElement>declarationBars[0], "#15ac51",
+      ((this.declarations.total.websites.conform -
+        this.declarations.currentYear.websites.conform +
+        this.declarations.total.apps.conform -
+        this.declarations.currentYear.apps.conform) /
+        totalDeclarations) *
+      100);
     this.fillBar(
       <HTMLElement>declarationBars[1],
       "#15ac51",
       ((this.declarations.currentYear.websites.conform +
         this.declarations.currentYear.apps.conform) /
         totalDeclarations) *
-        100
+      100
     );
-    this.fillBar(<HTMLElement>declarationBars[2], "#f3d609", 100);
+    this.fillBar(<HTMLElement>declarationBars[2], "#f3d609",
+      ((this.declarations.total.websites.partial -
+        this.declarations.currentYear.websites.partial +
+        this.declarations.total.apps.partial -
+        this.declarations.currentYear.apps.partial) /
+        totalDeclarations) *
+      100);
     this.fillBar(
       <HTMLElement>declarationBars[3],
       "#f3d609",
       ((this.declarations.currentYear.websites.partial +
         this.declarations.currentYear.apps.partial) /
         totalDeclarations) *
-        100
+      100
     );
-    this.fillBar(<HTMLElement>declarationBars[4], "#e90018", 100);
+    this.fillBar(<HTMLElement>declarationBars[4], "#e90018",
+      ((this.declarations.total.websites.not_conform -
+        this.declarations.currentYear.websites.not_conform +
+        this.declarations.total.apps.not_conform -
+        this.declarations.currentYear.apps.not_conform) /
+        totalDeclarations) *
+      100);
     this.fillBar(
       <HTMLElement>declarationBars[5],
       "#e90018",
       ((this.declarations.currentYear.websites.not_conform +
         this.declarations.currentYear.apps.not_conform) /
         totalDeclarations) *
-        100
+      100
     );
 
-    this.fillBar(<HTMLElement>declarationBars[6], "#15ac51", 100);
+    this.fillBar(<HTMLElement>declarationBars[6], "#15ac51",
+      ((this.declarations.total.websites.conform - this.declarations.currentYear.websites.conform) / totalDeclarationsWeb) * 100
+    );
     this.fillBar(
       <HTMLElement>declarationBars[7],
       "#15ac51",
-      (this.declarations.currentYear.websites.conform / totalDeclarations) * 100
+      (this.declarations.currentYear.websites.conform / totalDeclarationsWeb) * 100
     );
-    this.fillBar(<HTMLElement>declarationBars[8], "#f3d609", 100);
+    this.fillBar(<HTMLElement>declarationBars[8], "#f3d609", ((this.declarations.total.websites.partial - this.declarations.currentYear.websites.partial) / totalDeclarationsWeb) * 100);
     this.fillBar(
       <HTMLElement>declarationBars[9],
       "#f3d609",
-      (this.declarations.currentYear.websites.partial / totalDeclarations) * 100
+      (this.declarations.currentYear.websites.partial / totalDeclarationsWeb) * 100
     );
-    this.fillBar(<HTMLElement>declarationBars[10], "#e90018", 100);
+    this.fillBar(<HTMLElement>declarationBars[10], "#e90018", ((this.declarations.total.websites.not_conform - this.declarations.currentYear.websites.not_conform) / totalDeclarationsWeb) * 100);
     this.fillBar(
       <HTMLElement>declarationBars[11],
       "#e90018",
-      (this.declarations.currentYear.websites.not_conform / totalDeclarations) *
-        100
+      (this.declarations.currentYear.websites.not_conform / totalDeclarationsWeb) *
+      100
     );
 
-    this.fillBar(<HTMLElement>declarationBars[12], "#15ac51", 100);
+    this.fillBar(<HTMLElement>declarationBars[12], "#15ac51", ((this.declarations.total.apps.conform - this.declarations.currentYear.apps.conform) / totalDeclarationsApps) * 100);
     this.fillBar(
       <HTMLElement>declarationBars[13],
       "#15ac51",
-      (this.declarations.currentYear.apps.conform / totalDeclarations) * 100
+      (this.declarations.currentYear.apps.conform / totalDeclarationsApps) * 100
     );
-    this.fillBar(<HTMLElement>declarationBars[14], "#f3d609", 100);
+    this.fillBar(<HTMLElement>declarationBars[14], "#f3d609", ((this.declarations.total.apps.partial - this.declarations.currentYear.apps.partial) / totalDeclarationsApps) * 100);
     this.fillBar(
       <HTMLElement>declarationBars[15],
       "#f3d609",
-      (this.declarations.currentYear.apps.partial / totalDeclarations) * 100
+      (this.declarations.currentYear.apps.partial / totalDeclarationsApps) * 100
     );
     this.fillBar(
       <HTMLElement>declarationBars[16],
       "#e90018",
 
-      100
+      ((this.declarations.total.apps.not_conform - this.declarations.currentYear.apps.not_conform) / totalDeclarationsApps) * 100
     );
     this.fillBar(
       <HTMLElement>declarationBars[17],
       "#e90018",
-      (this.declarations.currentYear.apps.not_conform / totalDeclarations) * 100
+      (this.declarations.currentYear.apps.not_conform / totalDeclarationsApps) * 100
     );
 
     const totalBadges =
@@ -115,12 +145,26 @@ export class ObservatoryNumbersComponent implements OnInit {
       this.badges.total.apps.silver +
       this.badges.total.apps.bronze;
 
+    const totalBadgesWeb =
+      this.badges.total.websites.gold +
+      this.badges.total.websites.silver +
+      this.badges.total.websites.bronze;
+
+    const totalBadgesApps =
+      this.badges.total.apps.gold +
+      this.badges.total.apps.silver +
+      this.badges.total.apps.bronze;
+
     const badgesBars = document.querySelectorAll(".stamp .bar");
 
     this.fillBar(
       <HTMLElement>badgesBars[0],
       "#a87d00",
-
+      ((this.badges.total.websites.gold -
+        this.badges.currentYear.websites.gold +
+        this.badges.total.apps.gold -
+        this.badges.currentYear.apps.gold) /
+        totalBadges) *
       100
     );
     this.fillBar(
@@ -129,12 +173,16 @@ export class ObservatoryNumbersComponent implements OnInit {
       ((this.badges.currentYear.websites.gold +
         this.badges.currentYear.apps.gold) /
         totalBadges) *
-        100
+      100
     );
     this.fillBar(
       <HTMLElement>badgesBars[2],
       "#75797b",
-
+      ((this.badges.total.websites.silver -
+        this.badges.currentYear.websites.silver +
+        this.badges.total.apps.silver -
+        this.badges.currentYear.apps.silver) /
+        totalBadges) *
       100
     );
     this.fillBar(
@@ -143,12 +191,16 @@ export class ObservatoryNumbersComponent implements OnInit {
       ((this.badges.currentYear.websites.silver +
         this.badges.currentYear.apps.silver) /
         totalBadges) *
-        100
+      100
     );
     this.fillBar(
       <HTMLElement>badgesBars[4],
       "#bc7448",
-
+      ((this.badges.total.websites.bronze -
+        this.badges.currentYear.websites.bronze +
+        this.badges.total.apps.bronze -
+        this.badges.currentYear.apps.bronze) /
+        totalBadges) *
       100
     );
     this.fillBar(
@@ -157,41 +209,41 @@ export class ObservatoryNumbersComponent implements OnInit {
       ((this.badges.currentYear.websites.bronze +
         this.badges.currentYear.apps.bronze) /
         totalBadges) *
-        100
+      100
     );
 
-    this.fillBar(<HTMLElement>badgesBars[6], "#a87d00", 100);
+    this.fillBar(<HTMLElement>badgesBars[6], "#a87d00", (this.badges.total.websites.gold - this.badges.currentYear.websites.gold / totalBadgesWeb) * 100);
     this.fillBar(
       <HTMLElement>badgesBars[7],
       "#a87d00",
       (this.badges.currentYear.websites.gold / totalBadges) * 100
     );
-    this.fillBar(<HTMLElement>badgesBars[8], "#75797b", 100);
+    this.fillBar(<HTMLElement>badgesBars[8], "#75797b", (this.badges.total.websites.silver - this.badges.currentYear.websites.silver / totalBadgesWeb) * 100);
     this.fillBar(
       <HTMLElement>badgesBars[9],
       "#75797b",
       (this.badges.currentYear.websites.silver / totalBadges) * 100
     );
-    this.fillBar(<HTMLElement>badgesBars[10], "#bc7448", 100);
+    this.fillBar(<HTMLElement>badgesBars[10], "#bc7448", (this.badges.total.websites.bronze - this.badges.currentYear.websites.bronze / totalBadgesWeb) * 100);
     this.fillBar(
       <HTMLElement>badgesBars[11],
       "#bc7448",
       (this.badges.currentYear.websites.bronze / totalBadges) * 100
     );
 
-    this.fillBar(<HTMLElement>badgesBars[12], "#a87d00", 100);
+    this.fillBar(<HTMLElement>badgesBars[12], "#a87d00", (this.badges.total.apps.gold - this.badges.currentYear.apps.gold / totalBadgesApps) * 100);
     this.fillBar(
       <HTMLElement>badgesBars[13],
       "#a87d00",
       (this.badges.currentYear.apps.gold / totalBadges) * 100
     );
-    this.fillBar(<HTMLElement>badgesBars[14], "#75797b", 100);
+    this.fillBar(<HTMLElement>badgesBars[14], "#75797b", (this.badges.total.apps.silver - this.badges.currentYear.apps.silver / totalBadgesApps) * 100);
     this.fillBar(
       <HTMLElement>badgesBars[15],
       "#75797b",
       (this.badges.currentYear.apps.silver / totalBadges) * 100
     );
-    this.fillBar(<HTMLElement>badgesBars[16], "#bc7448", 100);
+    this.fillBar(<HTMLElement>badgesBars[16], "#bc7448", (this.badges.total.apps.bronze - this.badges.currentYear.apps.bronze / totalBadgesApps) * 100);
     this.fillBar(
       <HTMLElement>badgesBars[17],
       "#bc7448",
@@ -204,20 +256,16 @@ export class ObservatoryNumbersComponent implements OnInit {
     color: string,
     percentage: number
   ): void {
-    element.style.background = `-webkit-linear-gradient(left, ${color}, ${color} ${percentage}%, #dcdcdb ${percentage}%, #dcdcdb ${
-      100 - percentage
-    }%)`;
+    element.style.background = `-webkit-linear-gradient(left, ${color}, ${color} ${percentage}%, #dcdcdb ${percentage}%, #dcdcdb ${100 - percentage
+      }%)`;
 
-    element.style.background = `-moz-linear-gradient(left, ${color}, ${color} ${percentage}%, #dcdcdb ${percentage}%, #dcdcdb ${
-      100 - percentage
-    }%)`;
+    element.style.background = `-moz-linear-gradient(left, ${color}, ${color} ${percentage}%, #dcdcdb ${percentage}%, #dcdcdb ${100 - percentage
+      }%)`;
 
-    element.style.background = `-ms-linear-gradient(left, ${color}, ${color} ${percentage}%, #dcdcdb ${percentage}%, #dcdcdb ${
-      100 - percentage
-    }%)`;
+    element.style.background = `-ms-linear-gradient(left, ${color}, ${color} ${percentage}%, #dcdcdb ${percentage}%, #dcdcdb ${100 - percentage
+      }%)`;
 
-    element.style.background = `-linear-gradient(left, ${color}, ${color} ${percentage}%, #dcdcdb ${percentage}%, #dcdcdb ${
-      100 - percentage
-    }%)`;
+    element.style.background = `-linear-gradient(left, ${color}, ${color} ${percentage}%, #dcdcdb ${percentage}%, #dcdcdb ${100 - percentage
+      }%)`;
   }
 }
