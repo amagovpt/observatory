034b0b9458631d7768b0cc82d9448469056f20d7
Estriga97 <fc47839@alunos.fc.ul.pt>
2023-03-07 11:35:18 +0000

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -23,12 +23,12 @@ export class DataService {
   constructor(private readonly http: HttpClient) {
     const host = location.hostname;
 
-    if (host === "localhost") {
+    /*if (host === "localhost") {
       this.server = "http://localhost:3001";
     } else {
       this.server = "/api";
-    }
-
+    }*/
+    this.server = "http://10.55.37.16/api2";
     this.declarations = {
       total: {
         websites: {
