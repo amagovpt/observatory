945c96328bf82c1c6776662a1d732596278918d2
Filipe Dávila <filipedavila6@gmail.com>
2026-03-10 10:44:08 +0000

    feat: enhance i18n integration by merging translations from accessmonitor-rulesets

--- src/i18n.js
+++ src/i18n.js
@@ -4,24 +4,22 @@ import enJSON from './locale/English.json'
 import ptJSON from './locale/Portuguese.json'
 import moment from 'moment';
 import 'moment/locale/pt';
+import {translations} from "@a12e/accessmonitor-rulesets";
 i18n.use(initReactI18next).init({
  resources: {
-    en: { ...enJSON },
-    pt: { ...ptJSON },
+    en: { translation: { ...enJSON.translation, ...translations.en.translation } },
+    pt: { translation: { ...ptJSON.translation, ...translations.pt.translation } },
 },
  lng: "pt",
 });
 
-// Function to update Moment.js locale based on i18next language
 function updateMomentLocale() {
     const currentLanguage = i18n.language;
     moment.locale(currentLanguage);
 }
 
-// Listen for language change
 i18n.on('languageChanged', () => {
     updateMomentLocale();
 });
 
-// Initially set the locale
 updateMomentLocale();
